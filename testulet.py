8fe2dfe2bd809da48c85ba0831981128b75323d4
stefan <s.lang@student.utwente.nl>
2025-04-03 13:39:18 +0200

    testuletboss

---
+++ testulet.py
@@ -0,0 +1,10 @@
+import random
+listt = []
+for i in range(0, 100):
+    listt.append(random.randint(0, 100))
+for i in listt[:]:
+    if i < 32 or i > 41:
+        listt.remove(i)
+print(listt)
+
+    
